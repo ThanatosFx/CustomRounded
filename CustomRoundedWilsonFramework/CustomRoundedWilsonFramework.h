d4df70bbc91da936f75dad7c1ffff02a59ade54c
Dhanaryo Wilson <dhanaryowilson@gmail.com>
2020-10-20 15:30:53 +0700

    Initial Commit

---
+++ CustomRoundedWilsonFramework/CustomRoundedWilsonFramework.h
@@ -0,0 +1,18 @@
+//
+//  CustomRoundedWilsonFramework.h
+//  CustomRoundedWilsonFramework
+//
+//  Created by Dhanaryo Wilson on 20/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomRoundedWilsonFramework.
+FOUNDATION_EXPORT double CustomRoundedWilsonFrameworkVersionNumber;
+
+//! Project version string for CustomRoundedWilsonFramework.
+FOUNDATION_EXPORT const unsigned char CustomRoundedWilsonFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomRoundedWilsonFramework/PublicHeader.h>
+
+
